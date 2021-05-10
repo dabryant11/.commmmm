be0607d7e111f0ee88960968d06933fbe4bad810
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2021-05-09 17:59:35 -0700

    post landing page test

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+
 function Footer() {
   return (
-    <div className="footer">
-      <h5>&copy; Darien Bryant </h5>
+    <div>
+      <div className="footer">
+        <h5>&copy; Darien Bryant </h5>
+      </div>
     </div>
   );
 }
